c606efe9dbac5b876b77df06dc7618fafc39f41c
Narindra Peaks <Narindra Peaks>
2017-04-18 14:14:49 -0400

    initial layout stuff

---
+++ templates.js
@@ -0,0 +1,7 @@
+var templates = {
+	bulletin: "<div>This is where the bulletin will go</div",
+
+	grades: "<div> This is where the gradessheet will be </div>",
+
+	messages: "<div>I'mma makethis nice </div>"
+};

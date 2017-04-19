cd2c6748482c764bd71a45b31998fab568900bba
yefrem <yefrem@mit.edu>
2017-04-19 17:52:00 -0400

    fixing grades

--- templates.js
+++ templates.js
@@ -1,7 +1,48 @@
 var templates = {
 	bulletin: "<div>This is where the bulletin will go</div",
 
-	grades: "<div> This is where the gradessheet will be </div>",
+	grades: 
+"<div id='wrapper'>" +
+    "<div id='leftBar'>" +
+    	"<table id='overall'>" +
+    	"<caption id='gradetablecaption'>Overall Grades</caption>" +
+    	"<tr>" +
+    	"<th>Subject</th>" +
+    	"<th>Grade</th>" +
+    	"</tr>" +
+
+    	"<tr>" +
+    	"<td>Math</td>" +
+    	"<td>90%</td>" +
+    	"</tr>" +
+
+    	"<tr>" +
+    	"<td>Reading</td>" +
+    	"<td>80%</td>" +
+    	"</tr>" +
+
+    	"<tr>" +
+    	"<td>Science</td>" +
+    	"<td>100%</td>" +
+    	"</tr>" +
+
+    	"<tr>" +
+    	"<td>Art</td>" +
+    	"<td>75%</td>" +
+    	"</tr>" +
+
+    	"</table>" +
+    "</div>" +
+    "<div class='divider'></div>" +
+    "<div id='rightBar'>" +
+    	"<select>" +
+    		"<option>Math</option>" +
+    		"<option>Reading</option>" +
+    		"<option>Science</option>" +
+    		"<option>Art</option>" +
+    	"</select>" +
+    "</div>" +
+"</div>",
 
 	messages: 
 "<div class='section-title'>Messages " +

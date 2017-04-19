9509762378b37baf26bd1dcc7828c53395e390e4
Narindra Peaks <Narindra Peaks>
2017-04-19 02:18:08 -0400

    added more shit

--- templates.js
+++ templates.js
@@ -3,5 +3,12 @@ var templates = {
 
 	grades: "<div> This is where the gradessheet will be </div>",
 
-	messages: "<div>I'mma makethis nice </div>"
+	messages: 
+"<div class='section-title'>Messages " +
+"</div>"+
+"<div id='messages-container'>"+
+"  <div id='messages-list'>"+
+"  </div>"+
+"  <input type='text' id='messages-input'/>"+
+"</div>"
 };

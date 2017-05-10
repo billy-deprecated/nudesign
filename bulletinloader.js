c93585cd2dd44adaf9541bd5043fcf2d5e9154e7
yefrem <yefrem@mit.edu>
2017-05-10 02:30:06 -0400

    adding bulletin back

--- bulletinloader.js
+++ bulletinloader.js
@@ -105,7 +105,7 @@ commentStorage[0].push({"Avatar":{
   "src": "graphics/sara.png"
   },
   "Comment":{
-   "Date": "March 3rd, 2017 7:25:00 PM",
+   "Date": "March 10th, 2017 7:25:00 PM",
    "Text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
  }
 });
